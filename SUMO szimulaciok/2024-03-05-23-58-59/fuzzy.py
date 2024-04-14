6dba1b13af58246584ae1d2d5dba9bc23d1b1748
Miklo Jozsef <miklo.jozsef.peter@student.ms.sapientia.ro>
2024-04-14 17:19:04 +0300

    fuzzy control begin

--- SUMO szimulaciok/2024-03-05-23-58-59/fuzzy.py
+++ SUMO szimulaciok/2024-03-05-23-58-59/fuzzy.py
@@ -1,18 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 class IN_MEM:
     def __init__(self):
-        self.Width = 0.0
-        self.Center = [0.0] * 9
-        self.Dom = [0.0] * 9
+        self.Width1 = 0.0
+        self.Center1 = [0.0] * 5
+        self.Dom1 = [0.0] * 5
+        self.Width2 = 0.0
+        self.Center2 = [0.0] * 5
+        self.Dom2 = [0.0] * 5
 
 class OUT_MEM:
     def __init__(self):
-        self.Width = 0.0
-        self.Center = [0.0] * 9
+        self.value=0.0
 
 class Fuz_Sys:
     def __init__(self):
         self.Emem = IN_MEM()
         self.Edotmem = IN_MEM()
+        self.Imem = IN_MEM()
         self.Outmem = OUT_MEM()
 
 def MAX(a,b):
@@ -28,14 +34,21 @@ def MIN(a,b):
         return b
     
 def Fuzzy_Init(Fuzzy_System):
-    Fuzzy_System.Emem.Width = 0.05
-    Fuzzy_System.Edotmem.Width = 0.025
-    Fuzzy_System.Outmem.Width = 0.05
-    for i in range(1, 8):
-        Fuzzy_System.Emem.Center[i] = (i - 4) * Fuzzy_System.Emem.Width
-        Fuzzy_System.Edotmem.Center[i] = (i - 4) * Fuzzy_System.Edotmem.Width
-        Fuzzy_System.Outmem.Center[i] = (i - 4) * Fuzzy_System.Outmem.Width
-    
+    Fuzzy_System.Emem.Width1 = 10
+    Fuzzy_System.Emem.Width2 = 4
+    Fuzzy_System.Edotmem.Width1 = 10
+    Fuzzy_System.Edotmem.Width2 = 4
+    Fuzzy_System.Imem.Width1 = 10
+    Fuzzy_System.Imem.Width2 = 4
+
+    for i in range(0, 5):
+        Fuzzy_System.Emem.Center1[i]=5*(i+1)
+        Fuzzy_System.Emem.Center2[i]=2*(i)-4
+        Fuzzy_System.Edotmem.Center1[i]=5*(i+1)
+        Fuzzy_System.Edotmem.Center2[i]=2*-4
+        Fuzzy_System.Imem.Center1[i]=5*(i+1)
+        Fuzzy_System.Imem.Center2[i]=2*-4
+
 def LeftAll(u,w,c):
     if u<c:
         return 1.0
@@ -54,51 +67,82 @@ def Triangle(u,w,c):
     else:
         return max(0,(1-(c-u)/w))
     
-def Fuzzyify(U, Mem):
-    Mem.Dom[1] = LeftAll(U, Mem.Width, Mem.Center[1])
-    for i in range(2, 7):
-        Mem.Dom[i] = Triangle(U, Mem.Width, Mem.Center[i])
-    Mem.Dom[7] = RightAll(U, Mem.Width, Mem.Center[7])
+def Fuzzyify(U1, U2, Mem):
+    Mem.Dom1[0] = LeftAll(U1, Mem.Width1, Mem.Center1[0])
+    Mem.Dom2[0] = LeftAll(U2, Mem.Width2, Mem.Center2[0])
+    for i in range(1, 4):
+        Mem.Dom1[i] = Triangle(U1, Mem.Width1, Mem.Center1[i])
+        Mem.Dom2[i] = Triangle(U2, Mem.Width2, Mem.Center2[i])
+    Mem.Dom1[4] = RightAll(U1, Mem.Width1, Mem.Center1[4])
+    Mem.Dom2[4] = RightAll(U2, Mem.Width2, Mem.Center2[4])
 
-def Match(Emem, Edotmem, Pos):
-    for i in range(1, 8):
-        if Emem.Dom[i] != 0:
-            Pos[0] = i
-            break
-    for i in range(1, 8):
-        if Edotmem.Dom[i] != 0:
-            Pos[1] = i
-            break
+def Match(Emem, Edotmem, Imem, Pos):
 
-def Inf_Defuzz(Emem, Edotmem, Outmem, Pos):
-    Atot = 0
-    WAtot = 0
-    for I in range(1, 3):
-        for J in range(1, 3):
-            if (Pos[0] + I - 1 <= 7) and (Pos[1] + J - 1 <= 7):
-                Out_Index = Pos[0] + I - 1 + Pos[1] + J - 1
-                if Out_Index < 5:
-                    Out_Index = 5
-                elif Out_Index > 11:
-                    Out_Index = 11
-                Out_Index = Out_Index - 4
-                OutDom = MIN(Emem.Dom[Pos[0] + I - 1], Edotmem.Dom[Pos[1] + J - 1])
-                Area = 2 * Outmem.Width * (OutDom - (OutDom * OutDom) / 2)
-                Atot += Area
-                WAtot += Area * Outmem.Center[Out_Index]
+    Pos1 = [0]*5
+    for i in range(0,5):
+        if Emem.Dom1[i] != 0 and Emem.Dom2[i]:
+            Pos1[i]=min(Emem.Dom1[i], Emem.Dom2[i])
+        else:
+            Pos1[i]=0
 
-    return WAtot / Atot
+    #Sulyozott atlag
+    Pos[0]=(Pos1[0]*5+Pos1[1]*10+Pos1[2]*15+Pos1[3]*20+Pos1[4]*25)/(5+10+15+20+25)
+    print(Emem.Dom1, Emem.Dom2)
+    print(Pos1)
 
-def Fuzzy_Control(e, edot, Fuzzy_System):
-    Pos = [0] * 2
-    Fuzzyify(e, Fuzzy_System.Emem)
-    Fuzzyify(edot, Fuzzy_System.Edotmem)
-    Match(Fuzzy_System.Emem, Fuzzy_System.Edotmem, Pos)
-    return Inf_Defuzz(Fuzzy_System.Emem, Fuzzy_System.Edotmem, Fuzzy_System.Outmem, Pos)
+    Pos2 = [0]*5
+    for i in range(0,5):
+        if Edotmem.Dom1[i] != 0 and Edotmem.Dom2[i]:
+            Pos2[i]=min(Edotmem.Dom1[i], Edotmem.Dom2[i])
+        else:
+            Pos2[i]=0
+
+    Pos[1]=(Pos2[0]*5+Pos2[1]*10+Pos2[2]*15+Pos2[3]*20+Pos2[4]*25)/(5+10+15+20+25)
+    print(Pos2)
+    Pos3 = [0]*5
+    for i in range(0,5):
+        if Imem.Dom1[i] != 0 and Imem.Dom2[i]:
+            Pos3[i]=min(Imem.Dom1[i], Imem.Dom2[i])
+        else:
+            Pos3[i]=0
+    Pos[2]=(Pos3[0]*5+Pos3[1]*10+Pos3[2]*15+Pos3[3]*20+Pos3[4]*25)/(5+10+15+20+25)
+    print(Pos3)
+    print(Pos)
+
+#Sulyozott atlag
+def Inf_Defuzz(Pos):
+    res=max(Pos[0], Pos[1], Pos[2])
+    print(res)
+    if res <= 0.2:
+        return -5
+    elif res > 0.2 and res <= 0.4:
+        return -2
+    elif res > 0.4 and res <= 0.6:
+        return 0
+    elif res > 0.6 and res <= 0.8:
+        return 2
+    elif res > 0.8:
+        return 5
+    else:
+         return 0
+
+def Fuzzy_Control(e1, e2, edot1, edot2, inp1, inp2, Fuzzy_System):
+    Pos = [0] * 3
+    Fuzzyify(e1, e2, Fuzzy_System.Emem)
+    Fuzzyify(edot1, edot2, Fuzzy_System.Edotmem)
+    Fuzzyify(inp1, inp2, Fuzzy_System.Imem)
+    Match(Fuzzy_System.Emem, Fuzzy_System.Edotmem,Fuzzy_System.Imem, Pos)
+    return Inf_Defuzz(Pos)
 
 # Teszt
+'''
 Fuzzy_System = Fuz_Sys()
 Fuzzy_Init(Fuzzy_System)
-e = 0.5
-edot = 0.3
-print("Fuzzy control:", Fuzzy_Control(e, edot, Fuzzy_System))
+e1 = 10
+e2 = -2
+edot1 = 15
+edot2 = 2
+inp1 = 20
+inp2 = 1
+print("Fuzzy control:", Fuzzy_Control(e1, e2, edot1, edot2, inp1, inp2, Fuzzy_System))
+'''
--- SUMO szimulaciok/2024-03-05-23-58-59/sumorun.py
+++ SUMO szimulaciok/2024-03-05-23-58-59/sumorun.py
@@ -15,6 +15,75 @@ packTLSData = []
 packBigData = []
 step=0
 
+lane1_cars_prev=0
+lane2_cars_prev=0
+lane3_cars_prev=0
+lane4_cars_prev=0
+lane5_cars_prev=0
+
+traffic_light_ref="cluster_1987096989_257998255_26003945_7717709774_#1more"
+sequence=1
+sequence_time=50
+delta_t=0
+delta_t1=0
+delta_t2=0
+delta_t3=0
+next_sequence=0
+sequence1_cars_prev=0
+sequence2_cars_prev=0
+sequence3_cars_prev=0
+
+def sequence_control():
+        global sequence1_cars_prev
+        global sequence2_cars_prev
+        global sequence3_cars_prev
+
+        #Waiting cars by lane
+        lane1_0=traci.lanearea.getLastStepVehicleNumber(detector1_0)
+        lane1_1=traci.lanearea.getLastStepVehicleNumber(detector1_1)
+        lane1_2=traci.lanearea.getLastStepVehicleNumber(detector1_2)
+        lane2_0=traci.lanearea.getLastStepVehicleNumber(detector2_0)
+        lane3_0=traci.lanearea.getLastStepVehicleNumber(detector3_0)
+        lane3_1=traci.lanearea.getLastStepVehicleNumber(detector3_1)
+        lane3_2=traci.lanearea.getLastStepVehicleNumber(detector3_2)
+        lane4_0=traci.lanearea.getLastStepVehicleNumber(detector4_0)
+        lane4_1=traci.lanearea.getLastStepVehicleNumber(detector4_1)
+
+        print("Cars waiting in lane 1_0:"+str(lane1_0))
+        print("Cars waiting in lane 1_1:"+str(lane1_1))
+        print("Cars waiting in lane 1_2:"+str(lane1_2))
+        print("Cars waiting in lane 2_0:"+str(lane2_0))
+        print("Cars waiting in lane 3_0:"+str(lane3_0))
+        print("Cars waiting in lane 1_1:"+str(lane3_1))
+        print("Cars waiting in lane 1_2:"+str(lane3_2))
+        print("Cars waiting in lane 1_0:"+str(lane4_0))
+        print("Cars waiting in lane 1_1:"+str(lane4_1))
+
+        waiting_cars=lane1_0+lane1_1+lane1_2+lane2_0+lane3_0+lane3_1+lane3_2+lane4_0+lane4_1
+        print("All waiting cars:"+str(waiting_cars))
+
+        sequence1_cars=lane1_0+lane1_1+lane3_0+lane3_1
+        sequence2_cars=lane1_2+lane2_0+lane3_2
+        sequence3_cars=lane4_0+lane4_1
+
+        sequence1_change=sequence1_cars_prev-sequence1_cars
+        sequence2_change=sequence2_cars_prev-sequence2_cars
+        sequence3_change=sequence3_cars_prev-sequence3_cars
+
+
+        #Fuzzy control
+        Fuzzy_System = fz.Fuz_Sys()
+        fz.Fuzzy_Init(Fuzzy_System)
+        fuzzy_output=fz.Fuzzy_Control(sequence1_cars, sequence1_change, sequence2_cars, sequence2_change, sequence3_cars, sequence3_change, Fuzzy_System)
+
+        #Update previous waiting cars by sequence
+        sequence1_cars_prev=sequence1_cars
+        sequence2_cars_prev=sequence2_cars
+        sequence3_cars_prev=sequence3_cars
+
+        print(f"Fuzzy output: {fuzzy_output}")
+        return fuzzy_output
+
 while traci.simulation.getMinExpectedNumber()>0:
         print(f"Step {step}:")
        
@@ -25,33 +94,149 @@ while traci.simulation.getMinExpectedNumber()>0:
             
         ##----------MACHINE LEARNING CODES/FUNCTIONS HERE----------##
 
-        #Fuzzy logic
-        Fuzzy_System = fz.Fuz_Sys()
-        fz.Fuzzy_Init(Fuzzy_System)
+        '''
         traffic_light = "cluster_1936414352_1936414379_26003429_7516041220_#2more"
-        tl2 = "joinedS_cluster_1048507714_1528675438_248743350_248743351_#2more_cluster_248743346_3620518112_513613012_cluster_26003932_3620518113_3620518114"
-        tl3 = "GS_cluster_1016184195_8729367879_9418026652_9418026684_#1more"
-        #controlled lanes:('-948993200#1_0', '-948993200#1_0', '-948993200#1_0', '-948993200#1_0', '198516616#5_0', '198516616#5_0', '198516616#5_1', '198516616#5_1', '198516616#5_1', '-198516616#9_0', '-198516616#9_0', '-198516616#9_1', '-198516616#9_1', '-198516616#9_1')
-        
+
         detector1="laneAreaDetector1"
         detector2="laneAreaDetector2"
         detector3="laneAreaDetector3"
         detector4="laneAreaDetector4"
         detector5="laneAreaDetector5"
 
-        print("Cars waiting in lane 1:"+str(traci.lanearea.getLastStepVehicleNumber(detector1)))
-        print("Cars waiting in lane 2:"+str(traci.lanearea.getLastStepVehicleNumber(detector2)))
-        print("Cars waiting in lane 3:"+str(traci.lanearea.getLastStepVehicleNumber(detector3)))
-        print("Cars waiting in lane 4:"+str(traci.lanearea.getLastStepVehicleNumber(detector4)))
-        print("Cars waiting in lane 5:"+str(traci.lanearea.getLastStepVehicleNumber(detector5)))
-
-
+        
         if step<10 or step>40:
-                traci.trafficlight.setPhaseDuration(traffic_light, 5)
-                traci.trafficlight.setRedYellowGreenState(traffic_light, "GGGGrrrrrrrGGGGg")
+                #traci.trafficlight.setPhaseDuration(traffic_light, 5)
+                traci.trafficlight.setRedYellowGreenState(traffic_light, "rrrrrrGGGrrGGG")
         else:
-                traci.trafficlight.setPhaseDuration(traffic_light, 5)
-                traci.trafficlight.setRedYellowGreenState(traffic_light, "yyyyGGGGGGGGrrrr")
+                #traci.trafficlight.setPhaseDuration(traffic_light, 5)
+                traci.trafficlight.setRedYellowGreenState(traffic_light, "yyyyrrrGGyyyGG")
+
+        tls="258668307"
+        traci.trafficlight.setRedYellowGreenState(tls, "rr")
+        '''
+        ### Dozsa Gyorgy utca
+        '''
+        if step%5==0:
+
+                lane1_cars=traci.lanearea.getLastStepVehicleNumber(detector1)
+                lane2_cars=traci.lanearea.getLastStepVehicleNumber(detector2)
+                lane3_cars=traci.lanearea.getLastStepVehicleNumber(detector3)
+                lane4_cars=traci.lanearea.getLastStepVehicleNumber(detector4)
+                lane5_cars=traci.lanearea.getLastStepVehicleNumber(detector5)
+
+                print("Cars waiting in lane 1:"+str(lane1_cars))
+                print("Cars waiting in lane 2:"+str(lane2_cars))
+                print("Cars waiting in lane 3:"+str(lane3_cars))
+                print("Cars waiting in lane 4:"+str(lane4_cars))
+                print("Cars waiting in lane 5:"+str(lane5_cars))
+
+                lane1_change=lane1_cars-lane1_cars_prev
+                lane2_change=lane2_cars-lane2_cars_prev
+                lane3_change=lane3_cars-lane3_cars_prev
+                lane4_change=lane4_cars-lane4_cars_prev
+                lane5_change=lane5_cars=lane5_cars_prev
+
+                print("Cars waiting in lane 1 - prev:"+str(lane1_change))
+                print("Cars waiting in lane 2 - prev:"+str(lane2_change))
+                print("Cars waiting in lane 3 - prev:"+str(lane3_change))
+                print("Cars waiting in lane 4 - prev:"+str(lane4_change))
+                print("Cars waiting in lane 5 - prev:"+str(lane5_change))
+
+                #Fuzzy logic
+                Fuzzy_System = fz.Fuz_Sys()
+                fz.Fuzzy_Init(Fuzzy_System)
+
+                #input1=[lane1_cars/12, lane1_change/12]
+                #input2=[(lane2_cars+lane3_cars)/12, (lane2_change+lane3_change)/12]
+                #input3=[(lane4_cars+lane5_cars)/12, (lane4_change+lane5_change)/12]
+
+                #print(input1, input2)
+
+                output_fuzzy=fz.Fuzzy_Control(lane1_cars, lane2_cars, Fuzzy_System)
+                print("Fuzzy control:", output_fuzzy)
+
+                if output_fuzzy<0.5:
+                        traci.trafficlight.setRedYellowGreenState(traffic_light, "rrrrGGGGGGGGGG")
+                else:
+                        traci.trafficlight.setRedYellowGreenState(traffic_light, "GGGGrrrrrrrrrr")
+
+                lane1_cars_prev=lane1_cars
+                lane2_cars_prev=lane2_cars
+                lane3_cars_prev=lane3_cars
+                lane4_cars_prev=lane4_cars
+                lane5_cars_prev=lane5_cars
+        '''
+        
+        ### Reformatus kollegium utca
+        detector1_0="laneAreaDetector-43791591#1_0"
+        detector1_1="laneAreaDetector-43791591#1_1"
+        detector1_2="laneAreaDetector-43791591#1_2"
+        detector2_0="laneAreaDetector-954637017#1_0"
+        detector3_0="laneAreaDetector826606609#0_0"
+        detector3_1="laneAreaDetector826606609#0_1"
+        detector3_2="laneAreaDetector826606609#0_2"
+        detector4_0="laneAreaDetector23349324#4_0"
+        detector4_1="laneAreaDetector23349324#4_1"
+        
+        if step==next_sequence and sequence==1:
+                traci.trafficlight.setRedYellowGreenState(traffic_light_ref, "GGGrrrrrrrGGGrrrrrr")
+                if step>0:
+                        delta_t1=sequence_control()
+                else: 
+                        lane1_0=traci.lanearea.getLastStepVehicleNumber(detector1_0)
+                        lane1_1=traci.lanearea.getLastStepVehicleNumber(detector1_1)
+                        lane1_2=traci.lanearea.getLastStepVehicleNumber(detector1_2)
+                        lane2_0=traci.lanearea.getLastStepVehicleNumber(detector2_0)
+                        lane3_0=traci.lanearea.getLastStepVehicleNumber(detector3_0)
+                        lane3_1=traci.lanearea.getLastStepVehicleNumber(detector3_1)
+                        lane3_2=traci.lanearea.getLastStepVehicleNumber(detector3_2)
+                        lane4_0=traci.lanearea.getLastStepVehicleNumber(detector4_0)
+                        lane4_1=traci.lanearea.getLastStepVehicleNumber(detector4_1)
+
+                        print("Cars waiting in lane 1_0:"+str(lane1_0))
+                        print("Cars waiting in lane 1_1:"+str(lane1_1))
+                        print("Cars waiting in lane 1_2:"+str(lane1_2))
+                        print("Cars waiting in lane 2_0:"+str(lane2_0))
+                        print("Cars waiting in lane 3_0:"+str(lane3_0))
+                        print("Cars waiting in lane 1_1:"+str(lane3_1))
+                        print("Cars waiting in lane 1_2:"+str(lane3_2))
+                        print("Cars waiting in lane 1_0:"+str(lane4_0))
+                        print("Cars waiting in lane 1_1:"+str(lane4_1))
+
+                        waiting_cars=lane1_0+lane1_1+lane1_2+lane2_0+lane3_0+lane3_1+lane3_2+lane4_0+lane4_1
+                        print("All waiting cars:"+str(waiting_cars))
+
+                        sequence1_cars=lane1_0+lane1_1+lane3_0+lane3_1
+                        sequence2_cars=lane1_2+lane2_0+lane3_2
+                        sequence3_cars=lane4_0+lane4_1
+
+                        #Update previous waiting cars by sequence
+                        sequence1_cars_prev=sequence1_cars
+                        sequence2_cars_prev=sequence2_cars
+                        sequence3_cars_prev=sequence3_cars
+                        delta_t1=0
+                next_sequence+=sequence_time+delta_t1
+                sequence+=1
+
+        elif step==next_sequence and sequence==2:
+                #traci.trafficlight.setPhaseDuration(traffic_light_ref, sequence_time)
+                traci.trafficlight.setRedYellowGreenState(traffic_light_ref, "rrrgggggggrrrggrrrr")
+                delta_t2=delta_t1=sequence_control()
+                next_sequence+=sequence_time+delta_t2
+                sequence+=1
+
+        elif step==next_sequence and sequence==3:
+                #traci.trafficlight.setPhaseDuration(traffic_light_ref, sequence_time-5)
+                traci.trafficlight.setRedYellowGreenState(traffic_light_ref, "Grrrrrrrrrrrrrrgggg")
+                delta_t3=delta_t1=sequence_control()
+                next_sequence+=sequence_time+delta_t3
+                sequence=1
+        
+        #Szekvenciak
+        #traci.trafficlight.setRedYellowGreenState(traffic_light_ref, "GGGrrrrrrrGGGrrrrrr")
+        #traci.trafficlight.setRedYellowGreenState(traffic_light_ref, "Grrrrrrrrrrrrrrgggg")
+        #traci.trafficlight.setRedYellowGreenState(traffic_light_ref, "rrrgggggggrrrggrrrr")
+
 
         ##---------------------------------------------------------------##
         
